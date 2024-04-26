5145dc252cb2a6b40a6680147e873d62f746c03d
Oleg Pashkovsky <oleg@pashkovsky.me>
2024-04-26 14:49:19 +0300

    Add permissions

--- _ide_helper.php
+++ _ide_helper.php
@@ -18715,6 +18715,65 @@ namespace Illuminate\Database\Schema {
             }
     }
 
+namespace App\Http\Requests {
+            /**
+     * 
+     *
+     */        class StoreEmailRequest {
+            }
+            /**
+     * 
+     *
+     */        class StoreProfileRequest {
+            }
+            /**
+     * 
+     *
+     */        class StoreUserRequest {
+            }
+            /**
+     * 
+     *
+     */        class UpdateUserRequest {
+            }
+    }
+
+namespace Illuminate\Foundation\Http {
+            /**
+     * 
+     *
+     */        class FormRequest {
+            }
+    }
+
+namespace Illuminate\Routing {
+            /**
+     * 
+     *
+     */        class Route {
+                    /**
+         * 
+         *
+         * @see \Spatie\Permission\PermissionServiceProvider::registerMacroHelpers()
+         * @param mixed $roles
+         * @static 
+         */        public static function role($roles = [])
+        {
+                        return \Illuminate\Routing\Route::role($roles);
+        }
+                    /**
+         * 
+         *
+         * @see \Spatie\Permission\PermissionServiceProvider::registerMacroHelpers()
+         * @param mixed $permissions
+         * @static 
+         */        public static function permission($permissions = [])
+        {
+                        return \Illuminate\Routing\Route::permission($permissions);
+        }
+            }
+    }
+
 
 namespace  {
             class App extends \Illuminate\Support\Facades\App {}
--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
@@ -20,7 +21,7 @@ class User extends Authenticatable
     use HasUuids;
     use SoftDeletes;
     use UserEventsTrait;
-
+    use HasRoles;
 
     /**
      * @var string
